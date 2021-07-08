37e8e214047865ce23a398bce5c76a11d2ca8b07
jeremy <jeremy@edgeexponential.com>
2021-07-08 20:43:16 +0100

    modified chztest for better modularity

--- chztest.py
+++ chztest.py
@@ -6,16 +6,16 @@ import datetime
 
 
 #***************************************MOTOR SET UP****************************************
-pin_tturnclk=8
-pin_tmoveclk=38
-pin_tmovedir=36
-pin_noid7=15
-pin_noid10=13
-pin_noid12=12
-pin_noid14=11
+pin_tturnclk=36
+pin_tmoveclk=29
+pin_tmovedir=31
+pin_noid7=16
+pin_noid10=15
+pin_noid12=13
+pin_noid14=12
 pin_stepclk=37
 pin_stepdir=35
-pin_dcr=3
+pin_dcr=11
 pin_dcl=7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -41,7 +41,7 @@ GPIO.output(pin_dcr,GPIO.LOW)
         
 
 class step:
-    def init(freq=100,pin=37):
+    def init(freq=100,pin=pin_stepclk):
         global stepclk
         global oldfreq
         oldfreq=freq
@@ -57,26 +57,33 @@ class step:
         for i in range(oldfreq,newfreq,50*(2*(newfreq>oldfreq)-1)):
             stepclk.ChangeFrequency(i)
             time.sleep(.03)
-        oldfreq=newfreq    
+        oldfreq=newfreq
+        
     def rev():
         GPIO.output(35,1-GPIO.input(35))
     def stop():
-        pep.turn(100)
+        step.turn(1)
         stepclk.ChangeDutyCycle(0)
    
 class chz:
-    def mask(spin=11,duration=4.2):
-        dc.go(spin)#13%DC for warm dry shred, 11 for cold wet fresh
-        pep.turn(1000)
-        time.sleep(duration) #4.2?
-        pep.stop()
-        dc.go(0)
+    def mask(size=14,timeadj=0):
+        mask(size)
+        time.sleep(1)
+        table.turn(800)
+        dc.go(3.6*size)#13%DC for warm dry shred, 11 for cold wet fresh
+        step.turn(1000)
+        time.sleep(size*size/19.6+timeadj)     
+        step.stop()
         stop()
+        time.sleep(1)
+        dc.go(0)
+        mask(0)
+        
     def spiral(speed=3,feed=300,dist=1.83):
         inps=speed/6.28 #inch per second
         spsr=inps*1600 #steps per second-radius; multiply by radius for motor speed
         dc.go(20)
-        pep.turn(feed)
+        step.turn(feed)
         table.turn(500)
         time.sleep(1)
         for radius in [dist,2*dist,3*dist]:
@@ -84,16 +91,16 @@ class chz:
             table.turn(spsr/radius) #inps/circumference=rpm 
             time.sleep(radius/inps)#Wait for full rotation   
         table.stop()
-        pep.stop()
+        step.stop()
         dc.go(0)
         
 class table:
-    def init(pin1=8,pin2=38):
+    def init(pin_turn=pin_tturnclk,pin_move=pin_tmoveclk):
         global turnclk
-        turnclk=GPIO.PWM(pin1,500)
+        turnclk=GPIO.PWM(pin_turn,500)
         turnclk.start(0)
         global tranclk
-        tranclk=GPIO.PWM(pin2,1000)
+        tranclk=GPIO.PWM(pin_move,1000)
         tranclk.start(0)
         
     def move(dist,freq=20000,pin3=38): #Distance in inches that the table should move
@@ -125,13 +132,13 @@ class table:
         turnclk.ChangeDutyCycle(0)
 def mask(size):
         GPIO.output(pin_noid7,size==7)
-        GPIO.output(pin_noid10,size=10)
-        GPIO.output(pin_noid12,size=12)
-        GPIO.output(pin_noid14,size=14)
+        GPIO.output(pin_noid10,size==10)
+        GPIO.output(pin_noid12,size==12)
+        GPIO.output(pin_noid14,size==14)
 class dc:
     def init():
         global dcclk
-        dcclk=GPIO.PWM(7,500)
+        dcclk=GPIO.PWM(pin_dcr,500)
         dcclk.start(0)
     def go(speed=100):
         global dcclk
@@ -140,12 +147,14 @@ class dc:
 
 def stop():
     dc.go(0)
-    pep.stop()
+    mask(0)
+    step.stop()
     table.stop()
     
 
     
 #########################################INITIALIZE################################
 step.init()
+GPIO.output(pin_stepdir,1)
 table.init()
 dc.init()
