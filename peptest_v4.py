a95b3ada80f2937cbbee60a9aa39326b0b88541a
EdgeExponential <jeremy@EdgeExponential.com>
2021-12-21 14:49:39 -0500

    changes from pants machine

--- peptest_v4.py
+++ peptest_v4.py
@@ -18,7 +18,7 @@ ttran=stepper.motor(33,29)
 def in2step(inch):
     return int(inch*200*8/(3.14159*.411)) #Convert distance to steps on motor
 def home():
-    ttran.turn(24000)
+    ttran.turn(12000)
     while GPIO.input(lim_pin):pass
     ttran.stop()
 def hallblip(x):
@@ -33,18 +33,18 @@ def demo(size,r4r=True,pps=3):#peps/sec changes speed of the whole script
     pep_ratio=800*1.11 #steps/bladerev
     table_ratio=400     #steps/tablerev
     if r4r: #Ready-for-Revenue Pep
-        pep_ct={14:[1,6,6,12,12,24], #[24,12,12,6,6,1],
-                12:[20,10,10,5,0],
-                10:[15,9,6,1],
-                7: [9,6,1]}
-        row_ct={14:[-5.9,1.81,.99,1.28,.87,1.38], #[0.0,1.38,0.87,1.28,0.99,1.81],
+        pep_ct={14:[5,12,18,25],
+                12:[4,11,17,24],
+                10:[1,6,9,15],
+                7: [1,6,9]}
+        row_ct={14:[-4.8,1.5,1.5,1.6], 
                 12:[1.0,1.28,1.09,1.61,1.31],
-                10:[2.0,1.45,1.45,1.43],
-                7: [3.5,1.45,1.45]}
+                10:[-5.6,1.45,1.45,1.45],
+                7: [-5.6,1.45,1.45]}
     else: #Full Pep
         pep_ct={14:[3,9,14,21,26,32],
                 12:[3,9,14,21,26],
-                10:[23,16,9,4],
+                10:[4,9,16,23],
                 7: []}
         row_ct={14:[-5.3,1.12,1.12,1.12,1.12,1.12],
                 12:[-5.3,1.12,1.12,1.12,1.12],
@@ -52,7 +52,7 @@ def demo(size,r4r=True,pps=3):#peps/sec changes speed of the whole script
                 7: []}
 #Script   
     home()
-    translate=threading.Thread(target=ttran.step,args=(in2step(row_ct[size][0]),24000))
+    translate=threading.Thread(target=ttran.step,args=(in2step(row_ct[size][0]),24000,))
     if pep_ct[size][0]<4: #slow down blade and table for lower counts
         rotate=threading.Thread(target=tturn.ramp,args=(pps*table_ratio/pep_ct[size][1],))
     else: rotate=threading.Thread(target=tturn.ramp,args=(pps*table_ratio/pep_ct[size][0],))
@@ -65,19 +65,21 @@ def demo(size,r4r=True,pps=3):#peps/sec changes speed of the whole script
     
     
     for i in range(len(pep_ct[size])): #the business end
-        while GPIO.input(hall_pin):pass
         if i: #prevent duplicate initial index movement   
             translate=threading.Thread(target=ttran.step,args=(in2step(row_ct[size][i]),24000,))
             translate.start()
-        if pep_ct[size][i]>1:
+            table_mod=table_ratio
             w_rot=pps*table_ratio/pep_ct[size][i]
-            tturn.stop()
-            tturn.step(.95*table_ratio,w_rot)
-            tturn.turn(w_rot/2)
+        else:
+            table_mod=table_ratio*(pep_ct[size][i]-1)/pep_ct[size][i]
+            w_rot=pps*table_ratio/pep_ct[size][i+1]
+        tturn.stop()
+        tturn.step(table_mod,w_rot)
+        tturn.turn(w_rot/2)
         print('Target: ',pep_ct[size][i],'\tCount: ',n)
         n=0
+        while GPIO.input(hall_pin):pass
 
-    while GPIO.input(hall_pin):pass
     stop()
     home()
     print('Time Elapsed: ',round(time.time()-t0,2),'s,',n,'extra pep')
